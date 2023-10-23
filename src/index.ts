35b093ec35583013aba38c5399cd91d3b8ba12fc
pitufogranjero <pitufogranjero@gmail.com>
2023-10-23 10:36:59 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ const PORT: number = 3000;
 
 
 app.get('/',(req:Express.Request,res:Express.Response)=>{
-    res.send("Hola Mundo")
-})
+    res.send("<p>Hello world!</p>")
+});
 
-app.listen(PORT, () => {console.log(`App is listening at port ${PORT}`)})
+app.listen(PORT, () => {
+    console.log(`App is listening at port ${PORT}`)
+});
